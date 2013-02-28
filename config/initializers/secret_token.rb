70e7a01cf6580531fd1b79c7163bfda95035b5ab
jinwanlin <jinwanlin123@gmail.com>
2013-02-28 14:04:19 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weiwo::Application.config.secret_token = '858f148af1e8d7e28120d134f4a33bbfbecc2f79f7d6ff443fce50e4f3225b6d56671f02ceeaf21110ebbf56bcdee5e1de4bea339cc9a5c8816a16aa10af0130'
